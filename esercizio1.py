19192192f30d84a79afa618c98a9a391aeecde03
spampilaura <laurarossipn67@gmail.com>
2026-03-12 13:09:31 +0000

    inizio 4

--- esercizio1.py
+++ esercizio1.py
@@ -12,21 +12,24 @@ def positive_value(n) :
         while (n < 1) :
             return int(input("numero non valido, inserisci un numero valido DEVE ESSERE POSITIVO: "))
 """ punto 3) num. pari // 2, num. diversi *3+1, la lista finisce quando arriva a 1 oppure a 100 elementi """
-def generate_list(n) :
-    list = [n] # creo la lista 
-    while (n != 1 and len(list) <= 100) : # "len" è la lunghezza della lista, "!=" è diverso
+def generate_list(n, l) :
+   # l = [n] # creo la lista 
+    while (n != 1 and len(l) <= 100) : # "len" è la lunghezza della lista, "!=" è diverso
         if(n%2 == 0) : # se n è pari, n viene diviso per due 
             n = n//2 
         else : # se n è dispari, n viene moltiplicato per tre 
             n = n*3+1
-        list.append(n) # "append" aggiunge il numero alla lista 
-    return list
-    
+        l.append(n) # "append" aggiunge il numero alla lista 
+    return l
+def analizza_sequenza(l) : 
+    return sum(l) #somma gli elementi della lista 
 def main(): 
     result = int((input("scrivi un numero: ")))
     print(is_pari(result))
     print(positive_value(result))
-    print(generate_list(result))
+    l = [result]
+    print(generate_list(result, l))
+    print(analizza_sequenza(l))
 
 main()
 
