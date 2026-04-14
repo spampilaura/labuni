954e865b553ebe6d3b6844842c4b14f62005f316
spampilaura <laurarossipn67@gmail.com>
2026-04-14 12:25:54 +0000

    messaggio su cosa ho fatto

--- esercizio2.py
+++ esercizio2.py
@@ -21,12 +21,17 @@ Burnt green, and blue and white.
 '''
 
 
-count = 0
-a = testo.split('\n')
+count1 = 0 # count righe 
+count2 = 0 # count parole
+
+a = testo.split('\n') # divide il testo quando trova un a capo 
 print(len(a))
 for line in a : 
-    if len(line) > 0 : 
-        count +=1
-print("il numero delle righe è: ", count)
+    if len(line) > 0 : # se la lunghezza di "line" è maggiore di 0 fai: 
+        count1 += 1 # aggiungi 1 
+        words = line.split() # divide la linea quando trova uno spazio (per restituire una lista con parole)
+        count2 +=len(words) # len(words) conta elementi che ci sono in "words"
+print("il numero delle righe è: ", count1)
+print("il numero delle parole è: ", count2)
     #count +=1
 #print( "il numero di parole è: ", count)
